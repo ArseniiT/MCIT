dad9f6e538d2bc3936df236f3cf6af8ee878ab6b
ArseniiT <arseniit@gmail.com>
2018-10-26 09:17:37 -0400

    first commit

---
+++ Lecture3/index.js
@@ -0,0 +1,56 @@
+/* ****************************************  LECTURE 3   **************************************** */
+document.write(`/*******************************  LECTURE 3   *******************************/ <br><br><br>`);
+
+//--------------------------- TASK 1 -------------------------
+document.write(`//--------------------------- TASK 1 ------------------------- <br><br>`);
+
+function task1(b) {
+
+  document.write(` Even numbers between 1 to ${b}:<br>`);
+  for (let i = 0; i < b; i+=2) {
+    if(i != (b-2)) {
+      document.write(`${i}, `);
+    } else {
+      document.write(`${i}.`);
+    }
+    
+  }
+}
+task1(100);
+
+//--------------------------- TASK 2 -------------------------
+document.write(`<br><br>//--------------------------- TASK 2 -------------------------  <br><br>`);
+
+function task2(a) {
+  let b;
+
+  for (let i = 0; i < 10; i++) {
+    b = a * i;
+    document.write(`${a} x ${i} = ${b}`);
+    document.write("<br>")  
+  }
+}
+
+task2(10);
+
+//--------------------------- TASK 3 -------------------------
+document.write(`<br><br>//--------------------------- TASK 3 -------------------------  <br><br>`);
+
+function task3(a) {
+  let b = ''+a;
+  let sum = parseInt(b[0]) + parseInt(b[b.length - 1]);
+  document.write(`Sum of first and last digit of number ${a} is ${sum}.`);
+}
+
+task3(453);
+
+//--------------------------- TASK 4 -------------------------
+document.write(`<br><br>//--------------------------- TASK 4 -------------------------  <br><br>`);
+
+function task4() {
+  let a = 453;
+  let b = ''+a;
+  console.log(parseInt(b[0]) + parseInt(b[b.length - 1]));
+}
+
+task4();
